4fe24557ccad85b68a267f8a1e0345e17fb90abe
yuanxs <yuanxs@95szzl.cn>
2019-08-01 17:19:48 +0800

    201908011719

--- Date.go
+++ Date.go
@@ -1,17 +1,36 @@
 package goToolCommon
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	TimeFormat = "2006-01-02 15:04:05.000"
+)
 
 func GetDateStr(t time.Time) string {
-	return t.Format("2006-01-02")
+	return t.Format(TimeFormat[0:10])
 }
 
 func GetDateTimeStr(t time.Time) string {
-	return t.Format("2006-01-02 15:04:05")
+	return t.Format(TimeFormat[0:19])
 }
 
 func GetDateTimeStrWithMillisecond(t time.Time) string {
-	return t.Format("2006-01-02 15:04:05.000")
+	return t.Format(TimeFormat[0:23])
+}
+
+func ParseDateStr(s string) (time.Time, error) {
+	return time.Parse(TimeFormat[0:10], fmt.Sprintf("parse err,reqire format %s", TimeFormat[0:10]))
+}
+
+func ParseDateTimeStr(s string) (time.Time, error) {
+	return time.Parse(TimeFormat[0:19], fmt.Sprintf("parse err,reqire format %s", TimeFormat[0:19]))
+}
+
+func ParseDateTimeStrWithMillisecond(s string) (time.Time, error) {
+	return time.Parse(TimeFormat[0:23], fmt.Sprintf("parse err,reqire format %s", TimeFormat[0:23]))
 }
 
 func GetMillisecond(t time.Time) int64 {
