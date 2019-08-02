769db01d439013b236055891389286838b149a1b
yuanxs <yuanxs@95szzl.cn>
2019-08-02 12:53:24 +0800

    201908021253

--- Common.go
+++ Common.go
@@ -8,42 +8,11 @@ import (
 	"golang.org/x/text/encoding/simplifiedchinese"
 	"golang.org/x/text/transform"
 	"io/ioutil"
-	"math/rand"
 	"runtime"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
 )
-
-//生成随机数
-func RandInt(min int, max int) int {
-	if min == max {
-		return min
-	}
-	if min > max {
-		t := min
-		min = max
-		max = t
-	}
-	rand.Seed(getRandSeek())
-	return min + rand.Intn(max-min)
-}
-
-var randSeek = int64(1)
-var randMax = int64(1000000)
-var l sync.Mutex
-
-//获取随机数种子值
-func getRandSeek() int64 {
-	l.Lock()
-	if randSeek >= randMax {
-		randSeek = 0
-	}
-	randSeek++
-	l.Unlock()
-	return time.Now().UnixNano() + randSeek
-}
 
 //生成GUID
 func Guid() string {
